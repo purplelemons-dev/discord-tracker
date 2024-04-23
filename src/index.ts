2c03f48745e750e6928ea443c7082395014ce481
purplelemons-dev <ironbreaker2005@gmail.com>
2024-04-23 11:10:58 -0500

    Update index.ts to add video/mp4 content type for script.js

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,15 @@ app.get("/", (req, res) => {
     res.send(`<textarea style="height: 12rem; length: 48rem">${stuff}</textarea><p></p><script>document.querySelector("p").innerHTML = "You can access the link from <a href='/${uid}'>" + window.location + "${uid}</a>";</script>`);
 });
 
+
+app.get("/:id", (req, res) => {
+    // Send ./src/script.js as video/mp4
+    res.header("Content-Type", "video/mp4");
+    res.send(fs.readFileSync("src/script.js"));
+
+});
+
+/*
 app.get("/:id", (req, res) => {
     const { id } = req.params;
     if (!links[id]) {
@@ -30,19 +39,20 @@ app.get("/:id", (req, res) => {
     res.header("Content-Type", "image/png");
     res.send(fs.readFileSync("src/pixel.png"));
 
-    console.log(`CF-Connecting-IP: ${req.headers["CF-Connecting-IP"]}`);
+    console.log(`cf-connecting-ip: ${req.headers["cf-connecting-ip"]}`);
     console.log(`x-forwarded-for: ${req.headers["x-forwarded-for"]}`);
     console.log(`req.ip: ${req.ip}`);
-    console.log(`req.connection.remoteAddress: ${req.connection.remoteAddress}`);
+    console.log(`req.socket.remoteAddress: ${req.socket.remoteAddress}`);
     console.log("\nHeaders:", JSON.stringify(req.headers, null, 2))
 
-    const ip = req.headers["CF-Connecting-IP"] as string || req.headers["x-forwarded-for"] as string || req.ip;
+    const ip = req.headers["cf-connecting-ip"] as string || req.headers["x-forwarded-for"] as string || req.ip;
 
     if (ip && !links[id].includes(ip)) {
         links[id].push(ip);
     }
 
 });
+*/
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}/`);
